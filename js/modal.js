20ae22da7664045fac200afba66d2626007f7bf2
Nastullla <30768051+Nastullla@users.noreply.github.com>
2017-11-22 12:25:05 +0300

    Исправлены ошибки

--- js/modal.js
+++ js/modal.js
@@ -4,29 +4,29 @@ var form = popup.querySelector("form");
 
 var btnFeedback = document.querySelector(".feedback");
 var btnClose = document.querySelector(".close");
-var fildName = popup.querySelector("[name=name]");
+var fieldName = popup.querySelector("[name=name]");
 var mail = popup.querySelector("[name=mail]");
 var comment = popup.querySelector("[name=comment]");
 
 btnFeedback.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("modal-error");
-	popup.classList.remove("modal-hide");
-    overlay.classList.remove("modal-hide");
-    fildName.focus();
+  evt.preventDefault();
+  popup.classList.remove("modal-error");
+  popup.classList.remove("modal-hide");
+  overlay.classList.remove("modal-hide");
+  fieldName.focus();
 });
 
 btnClose.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.add("modal-hide");
-    overlay.classList.add("modal-hide");
+  evt.preventDefault();
+  popup.classList.add("modal-hide");
+  overlay.classList.add("modal-hide");
 });
 
 form.addEventListener("submit", function (evt) {
-    if (!fildName.value || !mail.value || !comment.value) {
-        evt.preventDefault();
-        popup.classList.remove("modal-error");
-        popup.offsetWidth = popup.offsetWidth;
-        popup.classList.add("modal-error");
-    }
+  if (!fieldName.value || !mail.value || !comment.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  }
 });    
